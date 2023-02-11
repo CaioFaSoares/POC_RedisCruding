402d6cf5f8ebf2ed7528b8e482cb26c5ed95b847
Caio <caiofasoares@gmail.com>
2023-02-10 22:11:13 -0300

    Initial Commit

---
+++ POC_RedisCruding/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  POC_RedisCruding
+//
+//  Created by Caio Soares on 10/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POC_RedisCruding/POC_RedisCrudingApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_RedisCrudingApp.swift
+//  POC_RedisCruding
+//
+//  Created by Caio Soares on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_RedisCrudingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
